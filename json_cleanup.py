9513c4b0a37c5b16e9331b4d7f528a6cf8a7e6ff
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2024-09-29 00:30:42 -0400

    cleanup

--- json_cleanup.py
+++ json_cleanup.py
@@ -6,25 +6,40 @@ def remove_inapplicable(source_file, destination_folder):
 
   with open(source_file, "r") as f:
     data = json.load(f)
-    del data["inapplicable"]  # Remove the inapplicable section
+    del data["inapplicable"]
     del data["incomplete"]
     del data["testEnvironment"]
 
   # Get the filename without extension
-  filename = source_file.split("/")[-1].split(".")[0]
+  filename = source_file.split("/")[-1].rsplit('.', 1)[0]
   destination_file = f"{destination_folder}/{filename}_cleaned.json"
 
   # Copy the modified data to a new file
   with open(destination_file, "w") as f:
     json.dump(data, f, indent=4)
 
-  print(f"Copied and modified file saved to: {destination_file}")
+  #print(f"Copied and modified file saved to: {destination_file}")
 
+def check_violations(json_file_path, target_path1, target_path2):
+  with open(json_file_path) as f:
+    data = json.load(f)
+    filename = json_file_path.split("/")[-1].rsplit('.', 1)[0]
 
-source_path = "./JSON"
-destination_folder = "./JSON cleaned"
-#source_path = "path/to/your/source.json"
+    # Check if "violations" key exists and is empty
+    if len(data['violations']) == 0:
+        destination_file = f"{target_path1}/{filename}.json"
+        with open(destination_file, "w") as f:
+          json.dump(data, f, indent=4)
+    else:
+      destination_file = f"{target_path2}/{filename}.json"
+      with open(destination_file, "w") as f:
+        json.dump(data, f, indent=4)
+                  
+source_path = "./Cleaned JSON/"
+destination_folder1 = "./Passed"
+destination_folder2 = "./Failed"
 items = os.listdir(source_path)
+
 for i in range(len(items)):
-    print(items[i])
-    remove_inapplicable("./JSON/"+items[i], destination_folder)
+    #remove_inapplicable(source_path+items[i], "./Cleaned JSON/")
+    check_violations(source_path+items[i], destination_folder1, destination_folder2)
