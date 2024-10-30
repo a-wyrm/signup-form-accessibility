cbd2459c67796324b06f352a6dc37cdd209f2313
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2024-10-29 22:31:43 -0400

    add severity graph

--- results_analysis.py
+++ results_analysis.py
@@ -1,11 +1,7 @@
 import json
 import os
+import csv
 
-# change source path also
-source_path = "./Cleaned JSON/Failed/WCAG A/"
-violation_counts = {}
-severity_counts = {}
-items = os.listdir(source_path)
 
 def process_severity(json_file_path, severity_counts):
   """
@@ -47,11 +43,66 @@ def process_violations(json_file_path, violation_counts):
             violation_counts[violation_id] = violation_counts.get(violation_id, 0) + 1
 
 
+def import_violations_and_severity_to_csv(json_file_path, violation_counts, severity):
+  """
+  Processes a list of violations and updates the violation counts dictionary.
 
-# change this to different functions
+  Args:
+    json_file_path: The path to the .JSON file.
+    violation_counts: A dictionary where keys are violation IDs and values are their counts.
+  """
+  with open(json_file_path) as f:
+    data = json.load(f)
+    violations = data.get("violations", [])
+
+    for violation in violations:
+        
+        violation_id = violation.get("id")
+        severity_id = violation.get("impact")
+
+        tags = violation.get("tags")
+
+        # remove other not in tags for just wcag2aa
+        if ('wcag2a' not in tags) and ('wcag2aa' not in tags):
+            continue
+        else:  
+            violation_counts[violation_id] = violation_counts.get(violation_id, 0) + 1
+            severity.setdefault(violation_id, severity_id)
+
+
+# change source path also
+source_path = "./Cleaned JSON/Failed/WCAG A/"
+violation_counts = {}
+severity = {}
+severity_counts = {}
+items = os.listdir(source_path)
+
+for i in range(len(items)):
+    import_violations_and_severity_to_csv(source_path+items[i], violation_counts, severity)
+    with open('violations_checker.csv', 'w+', newline='') as report_file:
+        fieldnames = ['Violation ID', 'Count', 'Severity']
+        writer = csv.DictWriter(report_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for violation_id, count in violation_counts.items():
+            writer.writerow({'Violation ID': violation_id, 'Count': count, 'Severity': severity[violation_id]})
+
+
+
+
+# comment these out if you want to count violations individually
+""" for i in range(len(items)):
+    process_violations(source_path+items[i], violation_counts)
+
+    with open('violations_checker.txt', 'w+') as report_file:
+        for violation_id, count in violation_counts.items():
+            report_file.write(f"{violation_id}: {count}\n") """
+
+""" # change this to different functions
 for i in range(len(items)):
     process_severity(source_path+items[i], severity_counts)
 
     with open('severity_report1.txt', 'w+') as report_file:
         for violation_id, count in severity_counts.items():
-            report_file.write(f"{violation_id}: {count}\n")
+            report_file.write(f"{violation_id}: {count}\n") """
+
+
